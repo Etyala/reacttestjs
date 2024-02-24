84bcb5035c3313e7a9f5058cb950d572aa9d5f1f
Deekshith <deekshith.etyala@gmail.com>
2024-02-25 00:19:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click on Below link to know about
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://about.me/deekshithetyala"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Amazing Developer
         </a>
       </header>
     </div>
