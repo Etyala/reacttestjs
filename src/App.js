15b6ab83c48c1d96bee668d14ec5940a1fc8783e
Deekshith <deekshith.etyala@gmail.com>
2024-03-07 10:18:06 +0530

    project cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Click on Below link to know about
         </p>
         <a
-          className="App-link"
           href="https://about.me/deekshithetyala"
           target="_blank"
           rel="noopener noreferrer"
